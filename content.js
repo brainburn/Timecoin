c3fe5ae4ea566769f12af43db81cfec121674643
Nikola Chonkov <nikola.chonkov@gmail.com>
2016-03-26 23:35:24 -0400

    now it does dynamically added nodes

--- content.js
+++ content.js
@@ -4,7 +4,21 @@
 var priceMatcher = /\$\d+\.\d{1,2}/g
 var matcher;
 
-while(matcher = priceMatcher.exec(document.body.innerHTML)){
-    console.log(matcher[0])
-    document.body.innerHTML = document.body.innerHTML.replace(matcher[0], "POOP");
-}
+var textNode;
+
+var observer = new MutationObserver(function(mutations, observer){
+    var treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT)
+    while (textNode = treeWalker.nextNode()){
+        if(textNode.parentElement.tagName !== 'SCRIPT'){
+            while(matcher = priceMatcher.exec(textNode.nodeValue)){
+                console.log(matcher[0])
+                textNode.nodeValue = textNode.nodeValue.replace(matcher[0], "POOP");
+            }
+        }
+    }
+})
+
+observer.observe(document, {
+  subtree: true,
+  attributes: true
+});
