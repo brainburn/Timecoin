a66ae9e752e7f72be79a6591165b27619379ad51
Nikola Chonkov <nikola.chonkov@gmail.com>
2016-03-30 10:01:04 -0400

    fixing spacing

--- content.js
+++ content.js
@@ -1,16 +1,16 @@
 function processPage(request){
-        var priceMatcher = /\$\d+\.\d{1,2}/g
-        var matcher;
-        var textNode;
-        var treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT)
-        while (textNode = treeWalker.nextNode()){
-            if(textNode.parentElement.tagName !== 'SCRIPT'){
-                while(matcher = priceMatcher.exec(textNode.nodeValue)){
-                    price_in_hours = parseFloat(matcher[0].substring(1))/parseFloat(request.dph)
-                    textNode.nodeValue = textNode.nodeValue.replace(matcher[0], price_in_hours.toFixed(2).toString() + " Hours");
-                }
+    var priceMatcher = /\$\d+\.\d{1,2}/g
+    var matcher;
+    var textNode;
+    var treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT)
+    while (textNode = treeWalker.nextNode()){
+        if(textNode.parentElement.tagName !== 'SCRIPT'){
+            while(matcher = priceMatcher.exec(textNode.nodeValue)){
+                price_in_hours = parseFloat(matcher[0].substring(1))/parseFloat(request.dph)
+                textNode.nodeValue = textNode.nodeValue.replace(matcher[0], price_in_hours.toFixed(2).toString() + " Hours");
             }
         }
+    }
 }
 
 chrome.runtime.onMessage.addListener(
